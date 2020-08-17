9a1d861a66f49ae62802ac16ed0fd71c76fc179e
Dayo Jiboye <dayojiboye2@gmail.com>
2020-08-17 22:47:44 +0100

    initial commit

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   res.setHeader(
     'Access-Control-Allow-Origin',
-    'https://dummy-movies-api.netlify.app/'
+    'https://dummy-movies-api.netlify.app'
   );
   res.setHeader('Access-Control-Allow-Methods', '*');
   next();
