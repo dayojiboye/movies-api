97326ce953a8b067cc270315a283cf631d086902
Dayo Jiboye <dayojiboye2@gmail.com>
2020-08-17 22:49:34 +0100

    initial commit

--- src/app.js
+++ src/app.js
@@ -24,10 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://dummy-movies-api.netlify.app'
-  );
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', '*');
   next();
 });
