8a9aa467dacaa58b4d572d4cb6247f160bdd50ee
Dayo Jiboye <dayojiboye2@gmail.com>
2020-08-17 19:50:10 +0100

    initial commit

--- src/app.js
+++ src/app.js
@@ -30,11 +30,6 @@ app.use('/.netlify/functions/app/movies', movies.filterMovies);
 app.use('/.netlify/functions/app/movies', addMovie);
 app.use('/.netlify/functions/app/movies', deleteMovie);
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
