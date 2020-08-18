e54b8d99bb5be729fe4db4e7f60074dbb863dfc9
Dayo Jiboye <dayojiboye2@gmail.com>
2020-08-18 01:05:49 +0100

    initial commit

--- src/app.js
+++ src/app.js
@@ -25,7 +25,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET',
+    'POST',
+    'OPTIONS',
+    'DELETE'
+  );
+  res.setHeader('Content-Type', 'application/json');
   next();
 });
 
